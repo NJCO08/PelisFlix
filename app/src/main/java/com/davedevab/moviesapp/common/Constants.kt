ac9a930c36d181ea0bc4f406366a66bd3cb0badd
Andrea Lancheros <andreavalentinalancherosnovoa@gmail.com>
2024-11-14 23:35:04 -0500

    update

--- app/src/main/java/com/davedevab/moviesapp/common/Constants.kt
+++ app/src/main/java/com/davedevab/moviesapp/common/Constants.kt
@@ -14,7 +14,7 @@ class Constants {
 
     companion object{
         const val BASE_URL = "https://api.themoviedb.org/3/"
-        const val API_KEY = "tu_api_key_12345"
+        const val API_KEY = "37b81667adb48dd2e2730e92caaaca07"
         const val IMAGE_ORIGINAL_BASE_URL = "https://image.tmdb.org/t/p/original/"
         const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
         const val URL_PARAM_API_KEY = "api_key"
